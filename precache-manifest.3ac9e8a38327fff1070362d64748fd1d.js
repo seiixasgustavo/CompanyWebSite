a08f18a45e53d8a0922949e6783afdee3cfdcb36
Gustavo Seixas <gustavo.nabakseixas@gmail.com>
2020-04-11 17:37:20 -0300

    Updates

---
+++ precache-manifest.3ac9e8a38327fff1070362d64748fd1d.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "790af9afcb7c76f80c4dc995ce1c8824",
+    "url": "/company-web-site/index.html"
+  },
+  {
+    "revision": "681ce83ecc384ef79b70",
+    "url": "/company-web-site/static/js/2.2f3a0fa4.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/company-web-site/static/js/2.2f3a0fa4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a3ae52121017641e48f4",
+    "url": "/company-web-site/static/js/main.0da3b844.chunk.js"
+  },
+  {
+    "revision": "98ea90fb4c7350689840",
+    "url": "/company-web-site/static/js/runtime-main.f60751b9.js"
+  },
+  {
+    "revision": "714a22b7fe7173bb4ed11d452c1760eb",
+    "url": "/company-web-site/static/media/blog-message.714a22b7.jpg"
+  },
+  {
+    "revision": "7fa40f08250534d8a86d210f1d53a8f7",
+    "url": "/company-web-site/static/media/bot-message.7fa40f08.svg"
+  },
+  {
+    "revision": "ba3b2091311cd2727baa1c8f52704d8c",
+    "url": "/company-web-site/static/media/computer-app.ba3b2091.svg"
+  },
+  {
+    "revision": "d8c3a1ac1b4c60f0c7757c9e5b49b918",
+    "url": "/company-web-site/static/media/contact-us.d8c3a1ac.jpg"
+  },
+  {
+    "revision": "5be9bde46141498a7cfd4902b92133b1",
+    "url": "/company-web-site/static/media/cplusplus.5be9bde4.svg"
+  },
+  {
+    "revision": "09ded83368cf3992e3ae9db00405b18a",
+    "url": "/company-web-site/static/media/flutter.09ded833.svg"
+  },
+  {
+    "revision": "8fc5db97f11653cec47d7f80c4886733",
+    "url": "/company-web-site/static/media/golang.8fc5db97.svg"
+  },
+  {
+    "revision": "13a096531e50277a3fb17460c39db221",
+    "url": "/company-web-site/static/media/java.13a09653.svg"
+  },
+  {
+    "revision": "61ed8b054178d0e8d1285ad8f29f4045",
+    "url": "/company-web-site/static/media/javascript.61ed8b05.svg"
+  },
+  {
+    "revision": "9007002dc8564bd2501eb54cd441b205",
+    "url": "/company-web-site/static/media/laptop.9007002d.svg"
+  },
+  {
+    "revision": "bfd974891baddb66128319420c205409",
+    "url": "/company-web-site/static/media/mobile-app.bfd97489.svg"
+  },
+  {
+    "revision": "4009eef3b3e291f296e71962c70895c5",
+    "url": "/company-web-site/static/media/react.4009eef3.svg"
+  },
+  {
+    "revision": "dacc011065a0949ef33535bc5b7b2685",
+    "url": "/company-web-site/static/media/reactJS-logo.dacc0110.svg"
+  },
+  {
+    "revision": "7a3bfdff02834b6e3c2cae0e78ccb564",
+    "url": "/company-web-site/static/media/vanilla-logo.7a3bfdff.svg"
+  }
+]);
