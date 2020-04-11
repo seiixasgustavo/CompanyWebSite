813c6836277912e560967afe8fd3c66d3541600f
Gustavo Seixas <gustavo.nabakseixas@gmail.com>
2020-04-11 17:02:57 -0300

    fixing homepage

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import {HashRouter} from "react-router-dom";
+import {BrowserRouter} from "react-router-dom";
 import * as serviceWorker from "./serviceWorker";
 
 import App from "./components/App";
 
 ReactDOM.render(
-  <HashRouter>
+  <BrowserRouter>
     <App />
-  </HashRouter>,
+  </BrowserRouter>,
   document.getElementById("root")
 );
 
